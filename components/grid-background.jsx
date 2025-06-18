5455e7e0f13618339a7dc0ad4e812f6365c1a4e9
omar <mirxaumar1212@gmail.com>
2025-06-18 16:08:19 +0500

    9

--- components/grid-background.jsx
+++ components/grid-background.jsx
@@ -1,16 +1,28 @@
 export default function GridBackground() {
   return (
-    <div className="absolute inset-0 z-0 overflow-hidden bg-[#0f1117]">
-      <div className="grid-container">
+    <div className="absolute inset-0 z-0 overflow-hidden bg-[#1d2027]">
+      <div className="grid-container" style={{ "--grid-color": "0, 0, 0" }}>
         <div className="w-full h-full pointer-events-none grid-layout">
-          {[...Array(14)].map((_, rowIndex) => (
+          {[...Array(15)].map((_, rowIndex) => (
             <div key={rowIndex} className="grid-row">
-              {[...Array(14)].map((_, colIndex) => (
+              {[...Array(15)].map((_, colIndex) => (
                 <div key={`${rowIndex}-${colIndex}`} className="grid-cell" />
               ))}
             </div>
           ))}
         </div>
+      </div>
+      <div
+        className="w-full h-full pointer-events-none grid-layout"
+        style={{ "--grid-color": "255, 255, 255" }}
+      >
+        {[...Array(15)].map((_, rowIndex) => (
+          <div key={rowIndex} className="grid-row">
+            {[...Array(15)].map((_, colIndex) => (
+              <div key={`${rowIndex}-${colIndex}`} className="grid-cell" />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
